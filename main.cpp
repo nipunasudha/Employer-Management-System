1507f81299ae41d1119c1526fe61079ddea04bcd
Nipuna Sudharaka <nipuna.14@cse.mrt.ac.lk>
2017-07-11 16:53:57 +0530

    Add successful command listen,parse,exception handling loop

--- main.cpp
+++ main.cpp
@@ -1,13 +1,30 @@
 #include<iostream>
+#include <sstream>
+#include <string>
 #include <fstream>
 #include "ManagementSystem.h"
 void cls(){
     cout << "\033[2J\033[1;1H";
 }
+vector<string> split(string str, char delimiter) {
+    vector<string> internal;
+    stringstream ss(str); // Turn the string into a stream.
+    string tok;
+    if (str.find(' ') == string::npos){
+        internal.push_back(str);
+        return internal;
+    }
+    while(getline(ss, tok, delimiter)) {
+        internal.push_back(tok);
+    }
+
+    return internal;
+}
+
 using namespace std;
 int main() {
-
-    string cmd;
+    string rawcmd;
+    vector<string> cmd;
     cls();
     cout << "=== Welcome to the Employee Management System ===\n----------------------------" << endl;
     cout << "Reading records from the file...\n" << endl;
@@ -19,17 +36,23 @@ int main() {
         managementSystem.addEmployee(s);
     }
     cout << "\nLoading data OK!\nPress Enter to Continue...\n----------------------------"<<endl;
-    getline(cin, cmd);
+    getline(cin, rawcmd);
     cls();
     while(true){ 
         cout << "=== Main menu ==="<<endl; 
-        getline(cin, cmd);
-        if(cmd=="q"){
-            break;
-        }else if(cmd=="p"){
-            
-        }else{
-            cout<<"Invalid command."<<endl;
+        try{
+            getline(cin, rawcmd);
+            cmd=split(rawcmd,' ');
+            if(cmd.at(0)=="q"){
+                break;
+            }else if(cmd.at(0)=="p"){
+                cout<<cmd.at(1)<<endl;
+            }else{
+                cout<<"Invalid command."<<endl;
+            }
+        }catch(...){
+
+            cout<<"Invalid command (E)"<<endl;
         }
     }
 
